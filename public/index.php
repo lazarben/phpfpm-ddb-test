1968be20bd6292678db0930b4bd8de90b4dc6c8c
lazarben <107434122+lazarben@users.noreply.github.com>
2023-04-06 16:17:11 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,11 +3,15 @@
     include '../aws.phar';
 
     use Aws\DynamoDb\DynamoDbClient;
+    use Aws\Credentials\CredentialProvider;
 
+    $provider = CredentialProvider::ecsCredentials();
+    $memoizedProvider = CredentialProvider::memoize($provider);
 
     $client = new DynamoDbClient([
         'region'  => 'us-east-1',
-        'version' => 'latest'
+        'version' => 'latest',
+        'credentials => $memoizedProvider
     ]);
 
     echo $client->listTables();
