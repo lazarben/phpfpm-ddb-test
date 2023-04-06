997e4f7a07e8d6c98450787806ed6ad5891c35ef
lazarben <107434122+lazarben@users.noreply.github.com>
2023-04-06 14:55:42 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
-    phpinfo();
+
+    include '../aws.phar';
+
+    use Aws\DynamoDb\DynamoDbClient;
+
+
+    $client = new DynamoDbClient([
+        'region'  => 'us-east-1',
+        'version' => 'latest'
+    ]);
+
+    echo $client->listTables();
 ?>
