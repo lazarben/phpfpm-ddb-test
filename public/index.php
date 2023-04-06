252e7b19c7954e66c6bf2844e48d1f3e1e23669d
lazarben <107434122+lazarben@users.noreply.github.com>
2023-04-06 17:59:19 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,14 +4,31 @@
 
     use Aws\DynamoDb\DynamoDbClient;
     use Aws\Credentials\CredentialProvider;
+    use Aws\Credentials\InstanceProfileProvider;
+    use Aws\Credentials\AssumeRoleCredentialProvider;
 
-    $provider = CredentialProvider::ecsCredentials();
-    $memoizedProvider = CredentialProvider::memoize($provider);
+    $profile = new InstanceProfileProvider();
+    $ARN = "arn:aws:iam::048559620512:role/InstanceRoleDynamoDB";
+    $sessionName = "ddb-session";
+
+    $assumeRoleCredentials = new AssumeRoleCredentialProvider([
+        'client' => new StsClient([
+            'region' => 'us-east-1',
+            'version' => 'latest',
+            'credentials' => $profile
+        ]),
+        'assume_role_params' => [
+            'RoleArn' => $ARN,
+            'RoleSessionName' => $sessionName,
+        ],
+    ]);
+
+    $provider = CredentialProvider::memoize($assumeRoleCredentials);
 
     $client = new DynamoDbClient([
         'region'  => 'us-east-1',
         'version' => 'latest',
-        'credentials' => $memoizedProvider
+        'credentials' => $provider
     ]);
 
     echo $client->listTables();
